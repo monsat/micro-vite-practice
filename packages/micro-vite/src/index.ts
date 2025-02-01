8ad63f76e4deeab6f55f073130470b893421f7d5
mon_sat <tnk@daijiten.info>
2025-02-01 14:25:36 +0900

    feat: micro-vite build

--- packages/micro-vite/src/index.ts
+++ packages/micro-vite/src/index.ts
@@ -1,1 +1,17 @@
-console.log('aa')
+import cac from 'cac'
+
+const cli = cac()
+
+cli.command('dev')
+  .action(() => {
+    console.log('dev serer start')
+  })
+
+cli.command('build')
+  .action(() => {
+    console.log('build start')
+  })
+
+cli.help()
+
+cli.parse()
