c1f25cd5291047bc08cabafd84e0019319f17dd5
mon_sat <tnk@daijiten.info>
2025-02-01 19:52:24 +0900

    feat(micro-vite): reload plugin
    localhost:3000 にアクセスると virtual script の console.log('bar') が動作していることが分かる

---
+++ packages/micro-vite/src/reloadPlugin.ts
@@ -0,0 +1,39 @@
+import type { Plugin } from 'rollup'
+import { parse } from 'node-html-parser'
+
+const virtualScriptId = '/@micro-vite:reload/script.js'
+const virtualScript = `
+  console.log('bar')
+`
+
+export const reload = (): Plugin => {
+  return {
+    name: 'micro-vite:reload',
+    // virtualScriptId に対しては virtualScriptId を返す（load で対応するため）
+    async resolveId(id: string) {
+      if (id === virtualScriptId) {
+        return virtualScriptId
+      }
+      return null
+    },
+    // virtualScriptId に対しては virtualScript を返す
+    async load(id: string) {
+      if (id === virtualScriptId) {
+        return virtualScript
+      }
+      return null
+    },
+    // HTML ファイルに virtualScriptId へのリンクのある script タグを挿入
+    async transform(code, id) {
+      if (!id.endsWith('.html')) {
+        return null
+      }
+
+      // HTML の head の末尾に virtualScriptId へのリンクのある script タグを挿入する
+      const doc = parse(code)
+      doc.querySelector('head')?.insertAdjacentHTML('beforeend', `<script src="${virtualScriptId}">`)
+      // doc.querySelector('head')?.insertAdjacentHTML('beforeend', `<script src="${virtualScriptId}"></script>`)
+      return doc.toString()
+    },
+  }
+}
