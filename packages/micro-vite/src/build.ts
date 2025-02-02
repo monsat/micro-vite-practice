8c5cb8a2092edf5a3d2acb1651e1965861d5dec1
mon_sat <tnk@daijiten.info>
2025-02-02 14:29:51 +0900

    feat(micro-vite): bundle for prod
    playgroundディレクトリでnpm run buildを実行すると、index.html以外にバンドルされたJavaScriptファイルがdistディレクトリに出力される

--- packages/micro-vite/src/build.ts
+++ packages/micro-vite/src/build.ts
@@ -1,11 +1,15 @@
 import * as path from 'node:path'
 import * as fs from 'node:fs/promises'
 import parse from 'node-html-parser'
+import { rollup } from 'rollup'
+import { getPlugins } from './plugins'
 
 const root = process.cwd()
 const dist = path.resolve(root, './dist')
 
 export const startBuild = async () => {
+  const plugins = getPlugins(false) // 本番環境用のプラグインを取得
+
   await fs.rm(dist, {
     recursive: true,
     force: true,
@@ -17,8 +21,23 @@ export const startBuild = async () => {
   const distIndexHtmlPath = path.resolve(dist, './index.html')
   // index.html を加工して dist/index.html に出力する
   await processHtml(indexHtmlPath, distIndexHtmlPath, async (src) => {
-    // スクリプトタグの src 属性を変更する
-    return src + '?processed'
+    // rollup でバンドルする
+    const bundle = await rollup({
+      input: path.resolve(root, `.${src}`),
+      plugins,
+    })
+
+    const { output } = await bundle.write({
+      dir: dist,
+      format: 'es',
+      entryFileNames: 'assets/[name].[hash].js',
+      chunkFileNames: 'assets/chunks/[name].[hash].js',
+    })
+
+    await bundle.close()
+
+    // 出力されたファイル名を返し、スクリプトタグの src 属性を書き換える
+    return `/${output[0].fileName}`
   })
 }
 
